b79fa1ebbe661a0c329e809fceddde0a79009966
Alex Smith <alexesmith@icloud.com>
2019-08-24 16:20:26 -0500

    have the comments component setup with the redux

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 
 class Comments extends Component {
+    state = {
+        comments: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            comments: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.dispatch({
+            type: 'ADD_COMMENTS',
+            payload: this.state
+        });
+        this.props.history.push('/review');
+    }
+
+
+
+
     render() {
         return (
             <>
                 <h1>Any comments you want to leave?</h1>
-                <div>
-                    
-                <button className="btn btn-secondary btn-lg checkoutBtn" onClick={() => this.props.history.push('/review')}>Next</button>
-                </div>
+                <form onSubmit={this.handleSubmit}>
+                    <input className="inputs" type="text" placeholder="Comments" onChange={this.handleChange} />
+                    <button className="btn btn-secondary btn-lg checkoutBtn" >Next</button>
+                </form>
             </>
 
 
@@ -21,4 +44,4 @@ class Comments extends Component {
 
 
 
-export default Comments;
+export default connect()(Comments);
