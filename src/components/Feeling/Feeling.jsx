2a19037974d8ac5288a47ee6cd1c05b796de7f04
Alex Smith <alexesmith@icloud.com>
2019-08-26 08:51:57 -0500

    setup material ui on button for feeling component

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 
 
@@ -15,7 +16,7 @@ class Feeling extends Component {
         })
     }
 
-    //if handle submit is on the form so i can enter value then hit enter or hit the next button
+    
     handleSubmit = (event) => {
         event.preventDefault();
         if (this.state.feeling === '') { alert('ENTER A INPUT!') }
@@ -32,14 +33,14 @@ class Feeling extends Component {
         return (
             <>
                 <h1>How Are You Feeling Today?</h1>
-                <form  onSubmit={this.handleSubmit}>
+                <form  >
                     <input className="inputs" type="radio" name="feeling" value="1"  onChange={this.handleRadio}/>1
                     <input className="inputs" type="radio" name="feeling" value="2"  onChange={this.handleRadio}/>2
                     <input className="inputs" type="radio" name="feeling" value="3"  onChange={this.handleRadio}/>3
                     <input className="inputs" type="radio" name="feeling" value="4"  onChange={this.handleRadio}/>4
                     <input className="inputs" type="radio" name="feeling" value="5"  onChange={this.handleRadio}/>5
                     <br />
-                    <button className="btn btn-secondary btn-lg checkoutBtn" >Next</button>
+                    <Button variant="contained" color="primary" onClick={this.handleSubmit} className="btn btn-secondary btn-lg checkoutBtn" >Next</Button>
                 </form>
             </>
 
