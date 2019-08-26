b7b422e201b0b0f57c7d88355c9ec306378fceba
Alex Smith <alexesmith@icloud.com>
2019-08-26 09:01:07 -0500

    added material ui to buttons on support comonent

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 class Support extends Component {
     state = {
@@ -35,17 +36,17 @@ class Support extends Component {
         return (
             <>
                 <h1>How well are you being supported?</h1>
-                <form onSubmit={this.handleSubmit}>
+                <form >
                     <input className="inputs" type="radio" name="support" value="1"  onChange={this.handleRadio}/>1
                     <input className="inputs" type="radio" name="support" value="2"  onChange={this.handleRadio}/>2
                     <input className="inputs" type="radio" name="support" value="3"  onChange={this.handleRadio}/>3
                     <input className="inputs" type="radio" name="support" value="4"  onChange={this.handleRadio}/>4
                     <input className="inputs" type="radio" name="support" value="5"  onChange={this.handleRadio}/>5
                     <br />
-                <button className="btn btn-secondary btn-lg checkoutBtn" >Next</button>
+                    <Button variant="contained" color="primary" onClick={this.handleSubmit} className="btn btn-secondary btn-lg checkoutBtn" >Next</Button>
                 </form>
                 {/* put the button outside the form so i can still hit return to enter the input or use the next button */}
-                <button className="btn btn-secondary btn-lg checkoutBtn" onClick={this.handleBack}>Back</button>
+                <Button variant="contained" color="secondary" className="btn btn-secondary btn-lg checkoutBtn" onClick={this.handleBack}>Back</Button>
             </>
 
 
