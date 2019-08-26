1b6c895f44d4668281eeee6583b3ba9137ae9579
Alex Smith <alexesmith@icloud.com>
2019-08-26 10:41:33 -0500

    changed the buttons in comments to material ui buttons

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 class Comments extends Component {
     state = {
@@ -32,13 +33,13 @@ class Comments extends Component {
         return (
             <>
                 <h1>Any comments you want to leave?</h1>
-                <form onSubmit={this.handleSubmit}>
+                <form >
                     <input className="inputs" type="text" placeholder="Comments" onChange={this.handleChange} />
                     <br />
-                    <button className="btn btn-secondary btn-lg checkoutBtn" >Next</button>
+                    <Button variant="contained" color="primary" onClick={this.handleSubmit} className="btn btn-secondary btn-lg checkoutBtn" >Next</Button>
                 </form>
                 {/* put the button outside the form so i can still hit return to enter the input or use the next button */}
-                <button className="btn btn-secondary btn-lg checkoutBtn" onClick={this.handleBack}>Back</button>
+                <Button variant="contained" color="secondary" className="btn btn-secondary btn-lg checkoutBtn" onClick={this.handleBack}>Back</Button>
             </>
 
 
