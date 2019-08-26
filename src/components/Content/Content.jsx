99a88b3b2eea3654caa5568d030c5d7ca63a0e97
Alex Smith <alexesmith@icloud.com>
2019-08-26 08:56:03 -0500

    material ui added to buttons in content component

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 
 class Content extends Component {
@@ -35,17 +36,17 @@ class Content extends Component {
         return (
             <>
                 <h1>How well are you understanding the content?</h1>
-                <form onSubmit={this.handleSubmit}>
+                <form >
                     <input className="inputs" type="radio" name="content" value="1"  onChange={this.handleRadio}/>1
                     <input className="inputs" type="radio" name="content" value="2"  onChange={this.handleRadio}/>2
                     <input className="inputs" type="radio" name="content" value="3"  onChange={this.handleRadio}/>3
                     <input className="inputs" type="radio" name="content" value="4"  onChange={this.handleRadio}/>4
                     <input className="inputs" type="radio" name="content" value="5"  onChange={this.handleRadio}/>5
                 <br/>
-                <button className="btn btn-secondary btn-lg checkoutBtn" >Next</button>
+                    <Button variant="contained" color="primary" onClick={this.handleSubmit} className="btn btn-secondary btn-lg checkoutBtn" >Next</Button>
                 </form>
                 {/* put the button outside the form so i can still hit return to enter the input or use the next button */}
-                <button className="btn btn-secondary btn-lg checkoutBtn" onClick={this.handleBack}>Back</button>
+                <Button variant="contained" color="secondary" className="btn btn-secondary btn-lg checkoutBtn" onClick={this.handleBack}>Back</Button>
             </>
 
 
